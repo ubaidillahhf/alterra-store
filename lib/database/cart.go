7255239417a3f3507cab006162944823703294a1
hafidhirsyad <hafidhkh97@gmail.com>
2021-07-28 17:25:56 +0700

    Add feature crud card, product and transaction

---
+++ lib/database/cart.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"alterra_store/configs"
+	"alterra_store/models/carts"
+)
+
+func CreateCart(createCart carts.CartStruct) (carts.Cart, error) {
+	var cartDb carts.Cart
+
+	cartDb.Name = createCart.Name
+	cartDb.Description = createCart.Description
+	cartDb.TotalPrice = createCart.TotalPrice
+	cartDb.TotalProduct = createCart.TotalProduct
+	cartDb.Sku = createCart.Sku
+	cartDb.ProductId = createCart.ProductId
+
+	err := configs.DB.Create(&cartDb).Error
+	if err != nil {
+		return cartDb, err
+	}
+	return cartDb, nil
+}
+
+func DeleteCart(productId int) (carts.Cart, error) {
+	var cartDb carts.Cart
+	err := configs.DB.Where("id = ?", productId).Delete(&cartDb).Error
+
+	if err != nil {
+		return cartDb, err
+	}
+	return cartDb, nil
+}
